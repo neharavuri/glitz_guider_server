85096a1a5226e526c87bdd5c4c1fcab995149899
Neha <ravuri.n@northeastern.edu>
2023-12-03 18:59:39 -0500

    more filters

--- Search.js
+++ Search.js
@@ -9,7 +9,24 @@ const Search = (app) => {
         start = end - 20;
       }
       axios.get("http://makeup-api.herokuapp.com/api/v1/products.json")
-      .then(response => res.json(response.data.slice(start, end).map((e) => ({id: e.id, name: e.name, brand: e.brand, type: e.product_type, image: e.api_featured_image}))));
+      .then(response => res.json(response.data.slice(start, end)
+      .filter(function(e) {
+        let tf = true;
+        if (req.query.brand) {
+          tf = tf && (e.brand == req.query.brand);
+        }
+        if (req.query.product_type) {
+          tf = tf && (e.product_type == req.query.product_type);
+        }
+        if (req.query.min) {
+          tf = tf && (e.price >= req.query.min);
+        }
+        if (req.query.max) {
+          tf = tf && (e.price <= req.query.max)
+        }
+        return tf;
+      })
+      .map((e) => ({price: e.price, id: e.id, name: e.name, brand: e.brand, type: e.product_type, image: e.api_featured_image}))));
     })
   }
 export default Search;

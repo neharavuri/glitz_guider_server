add6c0680107f2c4cc828e61db298e36aa091ad4
Neha <ravuri.n@northeastern.edu>
2023-12-03 19:04:11 -0500

    cors:

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import Search from "./Search.js";
-const app = express()
-Search(app)
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+Search(app);
 app.listen(4000)
